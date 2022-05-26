cc2b22a3d44012e9aff4dfb44dbd550e9d763b6b
jeremy0anderson <projects@jeremyanderson.dev>
2022-05-26 01:40:01 -0600

    ad

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import React from 'react';
-import {About, Navbar, Projects, Skills, Home} from './components'
+import {About, Navbar, Header, Projects, Skills, Home} from './components'
 import {Routes, Route} from 'react-router-dom';
-
+function AboutComponent() {
+    return (
+        <div>
+            <Header/>
+            <About/>
+        </div>
+    )
+}
+function ProjectsComponent(){
+    return (
+        <div>
+            <Header/>
+            <Projects/>
+        </div>
+    )
+}
+function SkillsComponent(){
+    return(
+        <div>
+            <Header/>
+            <Skills/>
+        </div>
+    )
+}
 function App() {
-  return (
+
+
+    return(
       <div className="App">
       <Navbar/>
       <Routes>
           <Route path="/" element={<Home/>}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/skills" element={<Skills/>}/>
-          <Route path="/projects" element={<Projects/>}/>
+          <Route path="/about" element={<AboutComponent/>}/>
+          <Route path="/skills" element={<SkillsComponent/>}/>
+          <Route path="/projects" element={<ProjectsComponent/>}/>
       </Routes>
       </div>
   );
--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,6 @@ import {About} from './About/About';
 import {Navbar} from './NavBar/Navbar';
 import {Skills} from './Skills/Skills';
 import {Projects} from "./Projects/Projects";
-import {Home} from './Home/Home';
+import {Home, Header} from './Home/Home';
 import {Contact} from './Contact/Contact';
-export {About, Navbar, Home, Skills, Contact, Projects}
+export {About, Navbar, Home, Skills, Header, Contact, Projects}
