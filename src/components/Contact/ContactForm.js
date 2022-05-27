d2f4dc80ad137473524e7f1576104e2e7b44dca4
jeremy0anderson <projects@jeremyanderson.dev>
2022-05-26 21:53:13 -0600

    form validation/errors complete

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from 'react';
-import {sendForm} from 'emailjs-com';
+import React, {useState} from 'react';
+import emailjs, {sendForm} from 'emailjs-com';
 import './ContactForm.css';
 import './Contact.css';
 export function ContactForm() {
@@ -8,20 +8,38 @@ export function ContactForm() {
   const [subject, setSubject] = useState('');
   const [message, setMessage] = useState('');
   const [response, setResponse] = useState('');
-
+  const [nameError, setNameError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [subjectError, setSubjectError] = useState('');
+  const [messageError, setMessageError] = useState('');
+emailjs.init("GcEAPFoFBFLNO769J");
+  const formHandler = async (e) => {
+      e.preventDefault();
+      await emailjs.send("service_lmzq4q7", "template_9ch7b0l",
+          {
+              sender: sender,
+              reply_to: email,
+              subject:subject,
+              message:message
+          },
+          "GcEAPFoFBFLNO769J", )
+          .then((res) => {
+              if (res.text === "OK")
+              setResponse("Message Sent!");
+              setSender('');
+              setMessage('');
+              setEmail('');
+              setSubject('');
+          }, (err)=>{
+              setResponse(err);
+          });
+  }
   return (
-      <form id="ContactForm" onSubmit={async (e) => {
-        e.preventDefault();
-        await sendForm("service_lmzq4q7", "template_9ch7b0l", {
-          sender: sender,
-          reply_to: email,
-          subject: subject,
-          to_name: "Jeremy",
-          message: message,
-        }, "GcEAPFoFBFLNO769J")
-            .then((res) => {
-                setResponse(res.text);
-                });
+      <div className="card">
+      <div className="form-container">
+      <form id="ContactForm" onSubmit={async(e)=>{
+          await formHandler(e);
+
       }}>
         <div className="columns">
           <div className="column">
@@ -31,16 +49,31 @@ export function ContactForm() {
               </label>
               <div className="control has-icons-left has-icons-right">
                 <input
-                    className={`input`}
+                    className="input"
                     type="text"
-                    placeholder="Your Name"
-                    onChange={(e) => setSender(e.target.value)}
+                    placeholder="Name"
+                    required
+                    minLength="2"
+                    maxLength="30"
+                    onBlur={(e)=>{
+                      if (!e.target.value.length ) {
+                        setNameError('Name Required');
+                      } else {
+                        setNameError("")
+                      }
+
+                    }}
+                    onChange={(e) => {
+                        setSender(e.target.value)
+                        setNameError('');
+                    }}
                     name="sender"
                 />
                 <span className="icon is-small is-left">
                   <i className="fas fa-user"/>
                 </span>
               </div>
+              <p className="error" style={{color:"red"}}>{nameError}</p>
             </div>
           </div>
           <div className="column">
@@ -53,16 +86,27 @@ export function ContactForm() {
                     className="input"
                     type="text"
                     placeholder="Email"
+                    onBlur={(e)=>{
+                      if (e.target.value.length && !e.target.value.match(/^([a-z0-9_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/)){
+                        setEmailError("Please Enter a Valid Email");
+                      } else if (!e.target.value.length){
+                        setEmailError("Email Required")
+                      } else{
+                        setEmailError("");
+                      }
+                    }}
                     onChange={(e) => {
                       setEmail(e.target.value)
+                        setEmailError('');
                     }}
-                    name="email"
+                    name="reply_to"
                 />
                 <span className="icon is-small is-left">
                   <i className="fas fa-envelope"/>
                 </span>
               </div>
             </div>
+            <p className="error">{emailError}</p>
           </div>
           <div className="column">
             <div className="field">
@@ -74,8 +118,17 @@ export function ContactForm() {
                     className={`input`}
                     type="text"
                     placeholder="Subject"
+                    onBlur={(e)=>{
+                        if (!e.target.value.length ) {
+                            setSubjectError('Subject Required');
+                        } else {
+                            setSubjectError("")
+                        }
+
+                    }}
                     onChange={(e) => {
                       setSubject(e.target.value)
+                      setSubjectError("");
                     }}
                     name="subject"
                 />
@@ -84,6 +137,7 @@ export function ContactForm() {
                 </span>
               </div>
             </div>
+              <p className="error">{subjectError}</p>
           </div>
         </div>
         <div>
@@ -95,11 +149,20 @@ export function ContactForm() {
               <textarea
                   className="textarea"
                   placeholder="Message"
+                  onBlur={(e)=>{
+                      if (!e.target.value.length ) {
+                          setMessageError('Message Required');
+                      } else {
+                          setMessageError("")
+                      }
+                  }}
                   onChange={(e) => {
                     setMessage(e.target.value)
+                    setMessageError('');
                   }}
                   name="message"/>
             </div>
+              <p className="error">{messageError}</p>
           </div>
         </div>
         <div className="buttonWrapper">
@@ -109,5 +172,7 @@ export function ContactForm() {
           </button>
         </div>
       </form>
+      </div>
+      </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,7 +13,7 @@ export function Header(){
                     <a className="button" href="#Projects" style={{ marginRight: '0.5rem' }}>
                         Projects
                     </a>
-                    <a className="button" target="_blank" rel="noopener noreferrer" download="" href="https://google.com">
+                    <a className="button" target="_blank" rel="noopener noreferrer" download="../../assets/images/Untitled.pdf">
                         Resume
                     </a>
                 </div>
