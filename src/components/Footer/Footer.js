6c20b0c228427c2ec85a98e8a0ac453c46597869
jeremy0anderson <projects@jeremyanderson.dev>
2022-05-30 21:14:17 -0600

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,27 +3,27 @@ import {useState, useEffect} from "react";
 
 const footerLinks = [{
     platform: 'https://github.com/jeremy0anderson',
-        link: 'https://cdn.svgporn.com/logos/github.svg'
+        link: 'https://cdn.svgporn.com/logos/github.svg',
+        key: "github"
 },
 {
     platform: 'https://linkedin.com/jeremyanderson-dev',
         link: 'https://cdn.svgporn.com/logos/linkedin.svg',
+        key: "linkedin"
 }];
 
 
 export function Footer(){
 
     const [profiles, setProfiles] = useState(<div/>)
-
-    useEffect(()=>{
-        setProfiles(footerLinks.map(({platform, link})=>{
-                return(
-                <a className="footer-section" href={platform}>
-                    <img className="image footer-logo" alt={platform} key={link} src={link}/>
+    useEffect(()=> {
+        setProfiles(footerLinks.map(({platform, link, key}) => {
+            return (
+                <a key={key} className="footer-section" href={platform}>
+                    <img className="image footer-logo" alt={platform} key={key + "1"} src={link}/>
                 </a>)
-        }))
-
-    })
+        }));
+    },[setProfiles]);
     return(
         <div id="Footer" className="footer">
             {profiles}
